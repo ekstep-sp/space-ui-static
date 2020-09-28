5dcf787cdcc28b54ef33bf8109e1fd0c77d5b364
AnjithaR <57887320+AnjithaR@users.noreply.github.com>
2020-09-28 10:31:42 +0530

    fixed the bug for border bottom in nav bar

--- library/ws-widget/collection/src/lib/btn-catalog/btn-catalog.component.ts
+++ library/ws-widget/collection/src/lib/btn-catalog/btn-catalog.component.ts
@@ -3,6 +3,8 @@ import { TreeCatalogService } from '../tree-catalog/tree-catalog.service'
 import { TFetchStatus } from '@ws-widget/utils'
 import { NSSearch } from '../_services/widget-search.model'
 import { NsWidgetResolver, WidgetBaseComponent } from '@ws-widget/resolver'
+import { Router, NavigationEnd } from '@angular/router'
+import { filter } from 'rxjs/internal/operators/filter'
 import { sortBy } from 'lodash'
 
 @Component({
@@ -13,13 +15,22 @@ import { sortBy } from 'lodash'
 export class BtnCatalogComponent extends WidgetBaseComponent
   implements NsWidgetResolver.IWidgetData<any> {
 
+  constructor(private catalogSvc: TreeCatalogService,
+
+              private readonly router: Router
+  ) {
+    super()
+    this.router.events.pipe(
+
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd)).subscribe(value => {
+        this.showBottomBorder = value.urlAfterRedirects.includes('/page/explore/')
+      })
+  }
+
   @Input() widgetData!: any
   catalogItems: NSSearch.IFilterUnitContent[] | null = null
   catalogFetchStatus: TFetchStatus = 'none'
-
-  constructor(private catalogSvc: TreeCatalogService) {
-    super()
-  }
+  showBottomBorder = false
 
   getCatalog() {
     this.catalogFetchStatus = 'fetching'
