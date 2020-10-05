b4916537102bc7f1237de83de8b70e5fdb66beb9
AnjithaR <57887320+AnjithaR@users.noreply.github.com>
2020-10-05 19:44:22 +0530

    resolved lint errors

--- src/app/routes/public/public-header/public-header.component.spec.ts
+++ src/app/routes/public/public-header/public-header.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { PublicHeaderComponent } from './public-header.component';
+import { PublicHeaderComponent } from './public-header.component'
 
 describe('PublicHeaderComponent', () => {
-  let component: PublicHeaderComponent;
-  let fixture: ComponentFixture<PublicHeaderComponent>;
+  let component: PublicHeaderComponent
+  let fixture: ComponentFixture<PublicHeaderComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PublicHeaderComponent ]
+      declarations: [PublicHeaderComponent],
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PublicHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(PublicHeaderComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
