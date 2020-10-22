7842d8c15d91b7b673c2b3710c979326433f3c8f
Rajesh572 <rajeshdora786@gmail.com>
2020-10-22 16:50:08 +0530

    content strip title size fixed

--- project/ws/app/src/lib/routes/social/routes/blogs/blogs-view/components/blog-view.component.ts
+++ project/ws/app/src/lib/routes/social/routes/blogs/blogs-view/components/blog-view.component.ts
@@ -272,4 +272,17 @@ export class BlogViewComponent implements OnInit {
       return ''
     }
   }
+
+   removeTags(str: any) {
+    if ((str === null) || (str === '')) {
+        return false
+    } else {
+        str = str.toString()
+    } 
+
+    // Regular expression to identify HTML tags in
+    // the input string. Replacing the identified
+    // HTML tag with a null string.
+    return str.replace(/(<([^>]+)>)/ig, '')
+}
 }
