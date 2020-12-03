34f6690f21c11e104cc43c59ab7b7c28fc27cfe5
rishabh kalra <rishabhkalra96@gmail.com>
2020-12-03 14:34:44 +0530

    user by org and views by org are now controlled by roles

--- project/ws/app/src/lib/routes/app-toc/services/app-toc.service.ts
+++ project/ws/app/src/lib/routes/app-toc/services/app-toc.service.ts
@@ -333,4 +333,30 @@ export class AppTocService {
       data,
     )
   }
+
+  isVisibileAccToRoles(allowedRoles: [string], notAllowedRoles: [string]) {
+    let finalAcceptance = true
+    if (this.configSvc.userRoles && this.configSvc.userRoles.size) {
+      if (notAllowedRoles.length) {
+        const rolesOK = notAllowedRoles.some(role => (this.configSvc.userRoles as Set<string>).has(role))
+        if (rolesOK) {
+          finalAcceptance = false
+        } else {
+          finalAcceptance = true
+        }
+      }
+      if (allowedRoles.length) {
+        const rolesOK = allowedRoles.some(role => (this.configSvc.userRoles as Set<string>).has(role))
+        if (!rolesOK) {
+          finalAcceptance = false
+        } else {
+          finalAcceptance = true
+        }
+      }
+      if (!notAllowedRoles.length && !allowedRoles.length) {
+        finalAcceptance = true
+      }
+    }
+    return finalAcceptance
+  }
 }
