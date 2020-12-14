e469bb7f6bc55d98d4e4a5a3e203dd3252c72c6a
Kamaleeswary <kamaleeanbu@gmail.com>
2020-12-14 14:49:00 +0530

    fixed scroll issues related tab specific

--- project/ws/app/src/lib/routes/app-toc/routes/toc-body-common-routing/toc-body-common-routing.component.ts
+++ project/ws/app/src/lib/routes/app-toc/routes/toc-body-common-routing/toc-body-common-routing.component.ts
@@ -1,38 +1,43 @@
-import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Router, ActivatedRoute, Data } from '@angular/router'
+import { Subscription } from 'rxjs'
+import { NsContent } from '@ws-widget/collection/src/public-api'
 
 @Component({
   selector: 'ws-app-toc-body-common-routing',
   templateUrl: './toc-body-common-routing.component.html',
   styleUrls: ['./toc-body-common-routing.component.scss'],
 })
-export class TocBodyCommonRoutingComponent implements OnInit {
+export class TocBodyCommonRoutingComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router) { }
+  private routeSubscription: Subscription | null = null
+  content: NsContent.IContent | null = null
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false
     }
-    // if (this.route && this.route.parent) {
-    //   this.routeSubscription = this.route.parent.data.subscribe((data: Data) => {
-    //     this.initData(data)
-    //   })
-    // }
+    if (this.route && this.route.parent) {
+      this.routeSubscription = this.route.parent.data.subscribe((data: Data) => {
+        this.content = data.content.data
+      })
+    }
     this.scroll()
   }
   scroll() {
     const overview = document.getElementById('overview') as HTMLElement
     const ataglance = document.getElementById('ataglance') as HTMLElement
     const contents = document.getElementById('contents') as HTMLElement
-    if (window.location.href.includes('overview') && overview) {
-      setTimeout(() => {
-        overview.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            })
-      },         500)
-    }
+    // if (window.location.href.includes('overview') && overview) {
+    //   setTimeout(() => {
+    //     overview.scrollIntoView({
+    //         behavior: 'smooth',
+    //         block: 'start',
+    //         })
+    //   },         500)
+    // }
     if (window.location.href.includes('ataglance') && ataglance) {
       setTimeout(() => {
         ataglance.scrollIntoView({
@@ -50,5 +55,9 @@ export class TocBodyCommonRoutingComponent implements OnInit {
       },         500)
     }
   }
-
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe()
+    }
+  }
 }
