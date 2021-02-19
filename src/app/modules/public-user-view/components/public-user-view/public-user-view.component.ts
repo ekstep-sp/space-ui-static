272b83ef79b8e67ea5d20706019d314d0a4047ea
Uma536 <55277404+Uma536@users.noreply.github.com>
2021-02-19 19:46:08 +0530

    Implemented users card list in userview

--- src/app/modules/public-user-view/components/public-user-view/public-user-view.component.ts
+++ src/app/modules/public-user-view/components/public-user-view/public-user-view.component.ts
@@ -1,13 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'ws-public-user-view',
   templateUrl: './public-user-view.component.html',
-  styleUrls: ['./public-user-view.component.scss']
+  styleUrls: ['./public-user-view.component.scss'],
 })
 export class PublicUserViewComponent implements OnInit {
 
   constructor() { }
+  cards = [
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+    {
+      name: 'Himshi Bachchas',
+      organisation: 'NIIT Limited',
+    },
+  ]
 
   ngOnInit() {
   }
--- src/app/modules/public-user-view/public-user-view.module.ts
+++ src/app/modules/public-user-view/public-user-view.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common'
 
 import { PublicUserViewRoutingModule } from './public-user-view-routing.module'
 import { PublicUserViewComponent } from './components/public-user-view/public-user-view.component'
-
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [PublicUserViewComponent],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatIconModule,
     PublicUserViewRoutingModule,
   ],
 })
