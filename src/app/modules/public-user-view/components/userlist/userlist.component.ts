60dcba045e82126ba65703bd73ca58a5dfdf499f
Uma536 <55277404+Uma536@users.noreply.github.com>
2021-02-20 18:17:57 +0530

    Implemented userlist card as a seperate component

---
+++ src/app/modules/public-user-view/components/userlist/userlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ws-userlist',
+  templateUrl: './userlist.component.html',
+  styleUrls: ['./userlist.component.scss'],
+})
+export class UserlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
