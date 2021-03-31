b89548e4f58a01f31d8f4db6e8a64051b9eb0bca
rishabh kalra <rishabhkalra96@gmail.com>
2021-03-31 17:12:26 +0530

    bug fix, uneccessary route

--- project/ws/app/src/lib/routes/user-dashboard/user-dashboard-routing.module.ts
+++ project/ws/app/src/lib/routes/user-dashboard/user-dashboard-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component'
-import { ContentMigrationDashboardComponent } from '../content-migration-ui/components/content-migration-dashboard/content-migration-dashboard.component'
 
 const routes: Routes = [
   {
     path: '',
     component: UserDashboardComponent,
-  },
-  {
-    path: '/migratecontent',
-    component: ContentMigrationDashboardComponent,
   },
 ]
 
