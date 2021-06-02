8ec07502d56a6a286e8bc6cdba8be46db0da0f61
Deepak Kumar <deepak@intelspace.io>
2021-06-02 08:19:40 +0530

    policy amendments

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t lexplatform.azurecr.io/fusion:prod-sprint-10.2.0 .
+docker build -t lexplatform.azurecr.io/fusion:prod-sprint-terms-10.2.0 .
 
 echo "docker build is completed !!!! Starting docker push"
 
-docker push lexplatform.azurecr.io/fusion:prod-sprint-10.2.0
+docker push lexplatform.azurecr.io/fusion:prod-sprint-terms-10.2.0
 
