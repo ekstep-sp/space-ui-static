d92da4cdaf30550ccf8618045f7c5a6bc012bd65
Thyagu <rajan.tech.86@gmail.com>
2022-04-06 20:37:43 +0530

    Space-v3 final Commit
    Space-v3 final Commit

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t lexplatform.azurecr.io/fusion:space-v3-sprint-1.3 .
+docker build -t lexplatform.azurecr.io/fusion:space-v3-sprint-4.6 .
 
 echo "docker build is completed !!!! Starting docker push"
 
-docker push lexplatform.azurecr.io/fusion:space-v3-sprint-1.3
+docker push lexplatform.azurecr.io/fusion:space-v3-sprint-4.6
 
